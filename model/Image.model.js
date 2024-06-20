c55abf3b9b9db52de42e890fe444e408cce23cb9
Utkarsh <utkarshsinghal369@gmail.com>
2024-06-20 15:47:19 +0530

    Addd annotation code

--- model/Image.model.js
+++ model/Image.model.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
+const annotationSchema = new mongoose.Schema({
+  x: { type: Number, required: true }, // x-coordinate of the top-left corner
+  y: { type: Number, required: true }, // y-coordinate of the top-left corner
+  width: { type: Number, required: true }, // Width of the annotation box
+  height: { type: Number, required: true }, // Height of the annotation box
+  content: { type: String }, // Optional: Content inside the annotation
+});
+
 const metadataSchema = new mongoose.Schema({
   name: { type: String },
   lastModifiedDate: { type: Date },
@@ -8,6 +16,7 @@ const metadataSchema = new mongoose.Schema({
   location: { type: String },
   byte: { type: Number },
   tags: { type: mongoose.Schema.Types.Mixed }, // Flexible field for additional metadata
+  annotations: [annotationSchema], // Array of annotation objects
 });
 
 const MetadataModel = mongoose.model("Metadata", metadataSchema);
